cd0e7d39b5fb4d0e6d19a28123f2131b923b8ce0
ARJUN REDDY <arjunreddy91100@gmail.com>
2026-08-31 11:12:41 -0500

    Add brand name generator script

---
+++ brand_new_generator.py
@@ -0,0 +1,6 @@
+
+# Brand Name Generator Project
+print("Welcome to the Brand Name Generator Project. This project is designed to help you create unique and catchy brand names for your business or product.")
+city = input("Which city did you grow up in?\n")
+pet = input("Enter the name of your pet:\n")
+print("Your brand name could be: " + city + " " + pet)
